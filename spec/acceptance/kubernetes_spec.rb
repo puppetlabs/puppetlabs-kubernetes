50e5b6d9b965a908c58cc69bb736644efae2895d
davejrt <2067825+davejrt@users.noreply.github.com>
2017-09-18 14:56:42 +1000

    Cloud 1373 (#9)
    * starting acceptance framework
    
    * working single node
    
    * working acceptance tests
    
    * fixing filenames for test nodes
    
    * resolving merge conflict and fixing supported ubuntu version

---
+++ spec/acceptance/kubernetes_spec.rb
@@ -0,0 +1,56 @@
+require 'spec_helper_acceptance'
+
+describe 'the Kubernetes module' do
+  context 'clean up before each test' do
+    before(:each) do
+    end
+
+
+    describe 'kubernetes class' do
+      context 'it should install the module and run' do
+        let(:pp) {"
+        class {'kubernetes':
+          controller => true,
+          bootstrap_controller => true,
+        }
+        "}
+
+        it 'should run' do
+          apply_manifest(pp, :catch_failures => true)
+        end
+
+        it 'should install kubectl' do
+          shell('kubectl', :acceptable_exit_codes => [0])
+        end
+
+        it 'should install kube-dns' do
+          shell('KUBECONFIG=/root/admin.conf kubectl get deploy --namespace kube-system kube-dns', :acceptable_exit_codes => [0])
+        end
+      end
+
+      context 'appliction deployment' do
+
+        it 'can deploy an application into a namespace and expose it' do
+          shell('sleep 300')
+          shell('KUBECONFIG=/root/admin.conf kubectl create -f /tmp/nginx.yml', :acceptable_exit_codes => [0]) do |r|
+            expect(r.stdout).to match(/namespace "nginx" created\ndeployment "my-nginx" created\nservice "my-nginx" created\n/)
+          end
+        end
+
+        it 'can access the deployed service' do
+          shell('sleep 120')
+          shell('curl 10.96.188.5', :acceptable_exit_codes => [0]) do |r|
+            expect(r.stdout).to match (/Welcome to nginx!/)
+          end
+        end
+
+        it 'can delete a deployment' do
+          shell('KUBECONFIG=/root/admin.conf kubectl delete -f /tmp/nginx.yml', :acceptable_exit_codes => [0]) do |r|
+            expect(r.stdout).to match(/namespace "nginx" deleted\ndeployment "my-nginx" deleted\nservice "my-nginx" deleted/)
+          end
+          shell('KUBECONFIG=/root/admin.conf kubectl get deploy --all-namespaces | grep nginx', :acceptable_exit_codes => [1])
+        end
+      end
+    end
+  end
+end
