b078861222b74d2213e6b0fb769153b996f0429c
Fabian Jucker <jucker@gyselroth.com>
2017-12-20 09:00:33 +0100

    add tests

--- spec/classes/service_spec.rb
+++ spec/classes/service_spec.rb
@@ -18,12 +18,13 @@ describe 'kubernetes::service', :type => :class do
         'controller' => false,
         'bootstrap_controller' => false,
         'etcd_ip' => '127.0.0.1',
+        'cni_network_provider' => 'https://foo.test',
       }
     end
 
    it { should contain_service('docker') }
    it { should contain_file('/etc/systemd/system/kubelet.service.d') }
-   it { should contain_file('/etc/systemd/system/kubelet.service.d/kubernetes.conf') }
+   it { should contain_file('/etc/systemd/system/kubelet.service.d/kubernetes.conf').with_content(/Environment="KUBELET_NETWORK_ARGS/) }
    it { should contain_exec('Reload systemd') }
    it { should contain_service('kubelet') }
   end
@@ -34,9 +35,23 @@ describe 'kubernetes::service', :type => :class do
         'bootstrap_controller' => true,
         'controller' => true,
         'etcd_ip' => '127.0.0.1',
+        'cni_network_provider' => 'https://foo.test',
       }
     end
 
     it { should contain_exec('Checking for the Kubernets cluster to be ready')}
+  end
+
+  context 'with cni_network_provider => none' do
+    let(:params) do
+      {
+        'controller' => false,
+        'bootstrap_controller' => false,
+        'etcd_ip' => '127.0.0.1',
+        'cni_network_provider' => 'none',
+      }
+    end
+
+   it { should contain_file('/etc/systemd/system/kubelet.service.d/kubernetes.conf').without_content(/Environment="KUBELET_NETWORK_ARGS/) }
   end
 end
