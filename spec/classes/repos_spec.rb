ab6d6336b0e5f1aadbfe647455c4d568a7f824c7
Ilia Mashkov <Ilia.Mashkov@softronic.se>
2018-04-10 15:29:08 +0200

    Update gpg key fingerprint used by K8s Ubuntu repo

--- spec/classes/repos_spec.rb
+++ spec/classes/repos_spec.rb
@@ -18,14 +18,21 @@ describe 'kubernetes::repos', :type => :class do
 
     let(:params) { { 'container_runtime' => 'docker' } }
 
-    it { should contain_apt__source('kubernetes') }
+    it { should contain_apt__source('kubernetes').with(
+      :ensure   => 'present',
+      :location => 'http://apt.kubernetes.io',
+      :repos    => 'main',
+      :release  => 'kubernetes-xenial',
+      :key      => { 'id' => '54A647F9048D5688D7DA2ABE6A030B21BA07F4FB', 'source' => 'https://packages.cloud.google.com/apt/doc/apt-key.gpg' }
+    ) }
+
     it { should contain_apt__source('docker').with(
-              :ensure   => 'present',
-              :location => 'https://apt.dockerproject.org/repo',
-              :repos    => 'main',
-              :release  => 'ubuntu-xenial',
-              :key      => { 'id' => '58118E89F3A912897C070ADBF76221572C52609D', 'source' => 'https://apt.dockerproject.org/gpg' }
-       ) }
+      :ensure   => 'present',
+      :location => 'https://apt.dockerproject.org/repo',
+      :repos    => 'main',
+      :release  => 'ubuntu-xenial',
+      :key      => { 'id' => '58118E89F3A912897C070ADBF76221572C52609D', 'source' => 'https://apt.dockerproject.org/gpg' }
+    ) }
 
   end
 
