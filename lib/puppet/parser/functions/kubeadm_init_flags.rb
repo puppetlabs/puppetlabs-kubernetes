bedf5f9becec048233af27a356f41786bfedf637
suckatrash <erik.hansen@puppet.com>
2018-10-17 09:41:00 -0700

    (maint) Fix function nil / undef conditionals for Puppet6
    This commit is needed to support Puppet 6, where we fixed this bug:
    https://tickets.puppetlabs.com/browse/PUP-9112
    
    We need to test for both an undef vaule (backwards compatible) and nil
    (in Puppet 6 and beyond)

--- lib/puppet/parser/functions/kubeadm_init_flags.rb
+++ lib/puppet/parser/functions/kubeadm_init_flags.rb
@@ -7,23 +7,23 @@ module Puppet::Parser::Functions
   newfunction(:kubeadm_init_flags, :type => :rvalue) do |args|
     opts = args[0] || {}
     flags = []
-    flags << "--apiserver-advertise-address '#{opts['apiserver_advertise_address']}'" if opts['apiserver_advertise_address'].to_s != 'undef'
-    flags << "--apiserver-bind-port '#{opts['apiserver_bind_port']}'" if opts['apiserver_bind_port'].to_s != 'undef'
-    flags << "--apiserver-cert-extra-sans '#{opts['apiserver_cert_extra_sans'].join(',')}'" if opts['apiserver_cert_extra_sans'].to_s != 'undef'
-    flags << "--cert-dir '#{opts['cert_dir']}'" if opts['cert_dir'].to_s != 'undef'
-    flags << "--config '#{opts['config']}'" if opts['config'].to_s != 'undef'
-    flags << "--cri-socket '#{opts['cri_socket']}'" if opts['cri_socket'].to_s != 'undef'
+    flags << "--apiserver-advertise-address '#{opts['apiserver_advertise_address']}'" if opts['apiserver_advertise_address'] && opts['apiserver_advertise_address'].to_s != 'undef'
+    flags << "--apiserver-bind-port '#{opts['apiserver_bind_port']}'" if opts['apiserver_bind_port'] && opts['apiserver_bind_port'].to_s != 'undef'
+    flags << "--apiserver-cert-extra-sans '#{opts['apiserver_cert_extra_sans'].join(',')}'" if opts['apiserver_cert_extra_sans'] && opts['apiserver_cert_extra_sans'].to_s != 'undef'
+    flags << "--cert-dir '#{opts['cert_dir']}'" if opts['cert_dir'] && opts['cert_dir'].to_s != 'undef'
+    flags << "--config '#{opts['config']}'" if opts['config'] && opts['config'].to_s != 'undef'
+    flags << "--cri-socket '#{opts['cri_socket']}'" if opts['cri_socket'] && opts['cri_socket'].to_s != 'undef'
     flags << '--dry-run' if opts['dry_run']
-    flags << "--feature-gates '#{opts['feature_gates'].join(',')}'" if opts['feature_gates'].to_s != 'undef'
-    flags << "--ignore-preflight-errors='#{opts['ignore_preflight_errors'].join(',')}'" if opts['ignore_preflight_errors'].to_s != 'undef'
-    flags << "--kubernetes-version '#{opts['kubernetes_version']}'" if opts['kubernetes_version'].to_s != 'undef'
-    flags << "--node-name '#{opts['node_name']}'" if opts['node_name'].to_s != 'undef'
-    flags << "--pod-network-cidr '#{opts['pod_network_cidr']}'" if opts['pod_network_cidr'].to_s != 'undef'
-    flags << "--service-cidr '#{opts['service_cidr']}'" if opts['service_cidr'].to_s != 'undef'
-    flags << "--service-dns-domain '#{opts['service_dns_domain']}'" if opts['service_dns_domain'].to_s != 'undef'
+    flags << "--feature-gates '#{opts['feature_gates'].join(',')}'" if opts['feature_gates'] && opts['feature_gates'].to_s != 'undef'
+    flags << "--ignore-preflight-errors='#{opts['ignore_preflight_errors'].join(',')}'" if opts['ignore_preflight_errors'] && opts['ignore_preflight_errors'].to_s != 'undef'
+    flags << "--kubernetes-version '#{opts['kubernetes_version']}'" if opts['kubernetes_version'] && opts['kubernetes_version'].to_s != 'undef'
+    flags << "--node-name '#{opts['node_name']}'" if opts['node_name'] && opts['node_name'].to_s != 'undef'
+    flags << "--pod-network-cidr '#{opts['pod_network_cidr']}'" if opts['pod_network_cidr'] && opts['pod_network_cidr'].to_s != 'undef'
+    flags << "--service-cidr '#{opts['service_cidr']}'" if opts['service_cidr'] && opts['service_cidr'].to_s != 'undef'
+    flags << "--service-dns-domain '#{opts['service_dns_domain']}'" if opts['service_dns_domain'] && opts['service_dns_domain'].to_s != 'undef'
     flags << '--skip-token-print' if opts['skip_token_print']
-    flags << "--token '#{opts['token']}'" if opts['token'].to_s != 'undef'
-    flags << "--token-ttl '#{opts['token_ttl']}'" if opts['token_ttl'].to_s != 'undef'
+    flags << "--token '#{opts['token']}'" if opts['token'] && opts['token'].to_s != 'undef'
+    flags << "--token-ttl '#{opts['token_ttl']}'" if opts['token_ttl'] && opts['token_ttl'].to_s != 'undef'
 
     flags.flatten.join(' ')
   end
--- lib/puppet/parser/functions/kubeadm_join_flags.rb
+++ lib/puppet/parser/functions/kubeadm_join_flags.rb
@@ -7,17 +7,17 @@ module Puppet::Parser::Functions
   newfunction(:kubeadm_join_flags, :type => :rvalue) do |args|
     opts = args[0] || {}
     flags = []
-    flags << "'#{opts['controller_address']}'" if opts['controller_address'].to_s != 'undef'
-    flags << "--config '#{opts['config']}'" if opts['config'].to_s != 'undef'
-    flags << "--cri-socket '#{opts['cri_socket']}'" if opts['cri_socket'].to_s != 'undef'
-    flags << "--discovery-file '#{opts['discovery_file']}'" if opts['discovery_file'].to_s != 'undef'
-    flags << "--discovery-token '#{opts['discovery_token']}'" if opts['discovery_token'].to_s != 'undef'
-    flags << "--discovery-token-ca-cert-hash 'sha256:#{opts['ca_cert_hash']}'" if opts['ca_cert_hash'].to_s != 'undef'
+    flags << "'#{opts['controller_address']}'" if opts['controller_address'] && opts['controller_address'].to_s != 'undef'
+    flags << "--config '#{opts['config']}'" if opts['config'] && opts['config'].to_s != 'undef'
+    flags << "--cri-socket '#{opts['cri_socket']}'" if opts['cri_socket'] && opts['cri_socket'].to_s != 'undef'
+    flags << "--discovery-file '#{opts['discovery_file']}'" if opts['discovery_file'] && opts['discovery_file'].to_s != 'undef'
+    flags << "--discovery-token '#{opts['discovery_token']}'" if opts['discovery_token'] && opts['discovery_token'].to_s != 'undef'
+    flags << "--discovery-token-ca-cert-hash 'sha256:#{opts['ca_cert_hash']}'" if opts['ca_cert_hash'] && opts['ca_cert_hash'].to_s != 'undef'
     flags << "--discovery-token-unsafe-skip-ca-verification '#{opts['skip_ca_verification']}'" if opts['skip_ca_verification']
-    flags << "--feature-gates '#{opts['feature_gates'].join(',')}'" if opts['feature_gates'].to_s != 'undef'
-    flags << "--ignore-preflight-errors '#{opts['ignore_preflight_errors'].join(',')}'" if opts['ignore_preflight_errors'].to_s != 'undef'
-    flags << "--node-name '#{opts['node_name']}'" if opts['node_name'].to_s != 'undef'
-    flags << "--token '#{opts['token']}'" if opts['token'].to_s != 'undef'
+    flags << "--feature-gates '#{opts['feature_gates'].join(',')}'" if opts['feature_gates'] && opts['feature_gates'].to_s != 'undef'
+    flags << "--ignore-preflight-errors '#{opts['ignore_preflight_errors'].join(',')}'" if opts['ignore_preflight_errors'] && opts['ignore_preflight_errors'].to_s != 'undef'
+    flags << "--node-name '#{opts['node_name']}'" if opts['node_name'] && opts['node_name'].to_s != 'undef'
+    flags << "--token '#{opts['token']}'" if opts['token'] && opts['token'].to_s != 'undef'
 
     flags.flatten.join(' ')
   end
