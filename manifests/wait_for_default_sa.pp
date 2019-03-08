090507a7075ff41fff77a2e404e3baa8b3b29d1a
Jo Rhett <jrhett@transcendinsights.com>
2019-03-08 14:08:17 -0800

    Make kubectl environment available in main class

--- manifests/wait_for_default_sa.pp
+++ manifests/wait_for_default_sa.pp
@@ -5,15 +5,17 @@ define kubernetes::wait_for_default_sa (
   Optional[Integer] $timeout   = undef,
   Optional[Integer] $tries     = 5,
   Optional[Integer] $try_sleep = 6,
+  Optional[Array] $env         = $kubernetes::environment,
 ) {
   $safe_namespace = shell_escape($namespace)
 
   # This prevents a known race condition https://github.com/kubernetes/kubernetes/issues/66689
   exec { "wait for default serviceaccount creation in ${safe_namespace}":
-    command   => "kubectl -n ${safe_namespace} get serviceaccount default -o name",
-    path      => $path,
-    timeout   => $timeout,
-    tries     => $tries,
-    try_sleep => $try_sleep,
+    command     => "kubectl -n ${safe_namespace} get serviceaccount default -o name",
+    path        => $path,
+    environment => $env,
+    timeout     => $timeout,
+    tries       => $tries,
+    try_sleep   => $try_sleep,
   }
 }
