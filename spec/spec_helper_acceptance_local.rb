564d85ce7bc29abb780660ee4f2384c741078302
sheena <sheena@puppet.com>
2019-12-16 10:12:49 +0000

    (maint) FM-8788 testfix for redhat and centos

--- spec/spec_helper_acceptance_local.rb
+++ spec/spec_helper_acceptance_local.rb
@@ -13,6 +13,9 @@ RSpec.configure do |c|
   c.before :suite do
    vmhostname = run_shell('hostname').stdout.strip
    vmipaddr = run_shell("ip route get 8.8.8.8 | awk '{print $NF; exit}'").stdout.strip
+   if os[:family] == 'redhat'
+     vmipaddr = run_shell("ip route get 8.8.8.8 | awk '{print $7; exit}'").stdout.strip
+   end
    vmos = os[:family]
 
    puts "Running acceptance test on #{vmhostname} with address #{vmipaddr} and OS #{vmos}"
@@ -101,7 +104,6 @@ EOS
     run_shell('DEBIAN_FRONTEND=noninteractive apt-get install ruby-dev --yes')
     run_shell('DEBIAN_FRONTEND=noninteractive apt-get install build-essential curl git m4 python-setuptools ruby texinfo libbz2-dev libcurl4-openssl-dev libexpat-dev libncurses-dev zlib1g-dev --yes')
     run_shell('apt-get -y install ruby-full')
-    #run_shell('gem install bundler concurrent-ruby semantic_puppet')
     if os[:family] == 'ubuntu'
       run_shell('sudo ufw disable')
     else
@@ -113,18 +115,9 @@ EOS
   if os[:family] == 'redhat'
     runtime = 'docker'
     cni = 'flannel'
-    run_shell('yum install -y ruby-devel git zlib-devel gcc-c++ lib yaml-devel libffi-devel make bzip2 libtool curl openssl-devel readline-devel')
+    run_shell('yum install -y  make bzip2 libtool curl openssl-devel readline-devel ruby-devel git zlib-devel gcc-c++', expect_failures: true)
     run_shell('yum install -y curl gpg gcc gcc-c++ make patch autoconf automake bison libffi-devel libtool patch readline-devel sqlite-devel zlib-devel openssl-devel')
     run_shell('gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB')
-    run_shell('curl -sSL https://get.rvm.io | bash -s stable').stdout
-    run_shell('rvm install 2.5.1')
-    run_shell('rvm use 2.5.1 --default')
-
-    run_shell('gem install bundler')
-    run_shell('gem install concurrent-ruby')
-    run_shell('gem install semantic_puppet')
-    run_shell('gem install deep_merge')
-
     run_shell('setenforce 0 || true')
     run_shell('swapoff -a')
     run_shell('systemctl stop firewalld && systemctl disable firewalld')
@@ -133,20 +126,12 @@ EOS
     run_shell('yum install -y docker-ce-18.06.3.ce-3.el7')
     run_shell("usermod -aG docker $(whoami)")
     run_shell('systemctl start docker.service')
-    if os[:operatingsystem] != 'redhat'
-      run_shell('yum install -y epel-release')
-    end
-    if os[:operatingsystem] == 'redhat'
-      run_shell('mv /etc/yum.repos.d/redhat.repo /etc/yum.repos.d/internal-mirror.repo')
-      run_shell('rpm -ivh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm')
-    end
-    run_shell('yum update -y -q')
+    
   end
-  #run_shell('cd  /etc/puppetlabs/code/modules/kubernetes;rm -rf Gemfile.lock;bundle install --path vendor/bundle')
   run_shell('curl -o go.tar.gz https://storage.googleapis.com/golang/go1.12.9.linux-amd64.tar.gz')
   run_shell('tar -C /usr/local -xzf go.tar.gz')
   run_shell('export PATH=$PATH:/usr/local/go/bin;go get -u github.com/cloudflare/cfssl/cmd/...')
-  run_shell("export PATH=$PATH:/usr/local/go/bin;export PATH=$PATH:/root/go/bin;cd /etc/puppetlabs/code/environments/production/modules/kubernetes/tooling;./kube_tool.rb -o #{vmos} -v 1.13.5 -r #{runtime} -c #{cni} -i \"#{vmhostname}:#{vmipaddr}\" -t \"#{vmipaddr}\" -a \"#{vmipaddr}\" -d true")
+  run_shell("export PATH=$PATH:/usr/local/go/bin;export PATH=$PATH:/root/go/bin;cd /etc/puppetlabs/code/environments/production/modules/kubernetes/tooling;/opt/puppetlabs/puppet/bin/ruby kube_tool.rb -o #{vmos} -v 1.13.5 -r #{runtime} -c #{cni} -i \"#{vmhostname}:#{vmipaddr}\" -t \"#{vmipaddr}\" -a \"#{vmipaddr}\" -d true")
   create_remote_file("hosts","/etc/hosts", hosts_file)
   create_remote_file("nginx","/tmp/nginx.yml", nginx)
   create_remote_file("hiera","/etc/puppetlabs/puppet/hiera.yaml", hiera)
@@ -169,7 +154,6 @@ EOS
     run_shell('echo "kubernetes::taint_master: false" >> /etc/puppetlabs/code/environments/production/hieradata/Ubuntu.yaml')
     run_shell('export KUBECONFIG=\'/etc/kubernetes/admin.conf\'')
   end
-
 
   if os[:family] == 'redhat'
     run_shell('sed -i /cni_network_provider/d /etc/puppetlabs/code/environments/production/hieradata/Redhat.yaml')
