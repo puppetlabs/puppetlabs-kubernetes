19efd00437afa4b046b40995e4865670bdd1f99c
JasonWhall <42138928+JasonWhall@users.noreply.github.com>
2020-09-02 11:08:28 +0100

    Updates docker yumrepo default - Current default value is not valid - Updates docs to reflect new value

--- manifests/init.pp
+++ manifests/init.pp
@@ -385,11 +385,11 @@
 #
 # [*docker_yum_baseurl*]
 #  The YUM repo URL for the Docker packages.
-#  Defaults to https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+#  Defaults to https://download.docker.com/linux/centos/7/x86_64/stable
 #
 # [*docker_yum_gpgkey*]
 #  The URL for the Docker yum repo gpg key
-#  Defaults to https://yum.dockerproject.org/gpg
+#  Defaults to https://download.docker.com/linux/centos/gpg
 #
 # [*docker_storage_driver*]
 #  Storage Driver to be added to `/etc/docker/daemon.json`
