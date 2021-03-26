dddb7340ba3b65ce1379baeb181fe7ec510391db
daiana <daiana.mezdrea@yahoo.com>
2021-03-26 12:22:00 +0200

    Test for k8s v.1.20 certification

--- plans/provision_cluster.pp
+++ plans/provision_cluster.pp
@@ -10,8 +10,12 @@ plan kubernetes::provision_cluster(
   #provision server machine, set role
   run_task('provision::provision_service', 'localhost',
     action => 'provision', platform => $gcp_image, vars => 'role: controller')
+    run_task('provision::provision_service', 'localhost',
+    action => 'provision', platform => $gcp_image, vars => 'role: controller')
   run_task('provision::provision_service', 'localhost',
     action => 'provision', platform => $gcp_image, vars => 'role: worker1')
   run_task('provision::provision_service', 'localhost',
     action => 'provision', platform => $gcp_image, vars => 'role: worker2')
+    run_task('provision::provision_service', 'localhost',
+    action => 'provision', platform => $gcp_image, vars => 'role: worker3')
 }
