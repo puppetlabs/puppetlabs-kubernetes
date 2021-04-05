7aa415ab92f832d552158cadb213d0ddb8f5f072
daiana <daiana.mezdrea@yahoo.com>
2021-04-05 17:54:12 +0300

    (maint) upaate workflows

--- plans/provision_cluster.pp
+++ plans/provision_cluster.pp
@@ -3,15 +3,16 @@
 # Provisions machines for integration testing
 #
 # @example
-#   kubernetes::provision_integration
+#   kubernetes::provision_cluster
 plan kubernetes::provision_cluster(
-  Optional[String] $gcp_image = 'centos-7',
+  Optional[String] $image_type = 'centos-7',
+  Optional[String] $provision_type = 'provision_service',
 ) {
   #provision server machine, set role
-  run_task('provision::provision_service', 'localhost',
-    action => 'provision', platform => $gcp_image, vars => 'role: controller')
-  run_task('provision::provision_service', 'localhost',
-    action => 'provision', platform => $gcp_image, vars => 'role: worker1')
-  run_task('provision::provision_service', 'localhost',
-    action => 'provision', platform => $gcp_image, vars => 'role: worker2')
+  run_task("provision::$provision_type", 'localhost',
+    action => 'provision', platform => $image_type, vars => 'role: controller')
+  run_task("provision::$provision_type", 'localhost',
+    action => 'provision', platform => $image_type, vars => 'role: worker1')
+  run_task("provision::$provision_type", 'localhost',
+    action => 'provision', platform => $image_type, vars => 'role: worker2')
 }
--- plans/puppetserver_setup.pp
+++ plans/puppetserver_setup.pp
@@ -1,9 +1,25 @@
+# @summary Provisions machines
+#
+# Puppet Server Setup
+#
+# @example
+#   kubernetes::puppetserver_setup
 plan kubernetes::puppetserver_setup(
+  Optional[String] $collection = 'puppet7-nightly'
 ) {
   $puppet_server =  get_targets('*').filter |$n| { $n.vars['role'] == 'controller' }
-  $puppet_server_string = $puppet_server[0].name
-  # install pe server
-  run_task('provision::install_puppetserver', $puppet_server)
+
+  # get facts
+  $puppet_server_facts = facts($puppet_server[0])
+  $platform = $puppet_server_facts['platform']
+
+  # install puppet server
+  run_task(
+    'provision::install_puppetserver',
+    $puppet_server,
+    'install and configure server',
+    { 'collection' => $collection, 'platform' => $platform }
+  )
 }
 
 
