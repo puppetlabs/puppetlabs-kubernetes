d67dc90e67f403b95d9670332bc85f164689adf1
Tomas Barton <barton.tomas@gmail.com>
2022-07-20 11:39:43 +0200

    Rewrite command line arguments parsing
    Use less code to do the same thing. Allow reading arguments from ENV
    variables.
    RSA 2048 is considered too weak for CA cert

--- tooling/kube_tool/other_params.rb
+++ tooling/kube_tool/other_params.rb
@@ -3,49 +3,44 @@ require 'securerandom'
 
 class OtherParams
 
-
-
-  def OtherParams.create(os, version, container_runtime, cni_provider, cni_provider_version, etcd_initial_cluster, etcd_ip, api_address, install_dashboard)
-    if install_dashboard.match('true')
-       install = true
-    else
-       install = false
-    end
-
+  def OtherParams.create(opts)
+    version = opts[:version]
+    container_runtime = opts[:container_runtime]
     kubernetes_minor_release = version.match(/(\d+\.)(\d+)/)[0]
 
-    if os.downcase.match('debian')
-      kubernetes_package_version = "#{version}-00"
-    elsif os.downcase.match('redhat')
-      kubernetes_package_version = version
+    kubernetes_package_version = case opts[:os].downcase
+    when 'debian'
+      "#{version}-00"
+    when 'redhat'
+      version
+    else
+      version
     end
 
-    if cni_provider.match('weave')
-       cni_network_provider = "https://cloud.weave.works/k8s/net?k8s-version=#{version}"
-       cni_pod_cidr = '10.32.0.0/12'
-    elsif
-       cni_provider.match('flannel')
-       cni_network_provider = 'https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml'
-       cni_pod_cidr = '10.244.0.0/16'
-    elsif cni_provider.match('calico')
-       cni_network_provider = "https://docs.projectcalico.org/archive/#{cni_provider_version}/manifests/calico.yaml"
-       cni_pod_cidr = '192.168.0.0/16'
-    elsif cni_provider.match('calico-tigera')
-       cni_network_preinstall = "https://docs.projectcalico.org/manifests/tigera-operator.yaml"
-       cni_network_provider = "https://docs.projectcalico.org/manifests/custom-resources.yaml"
-       cni_pod_cidr = '192.168.0.0/16'
-    elsif cni_provider.match('cilium')
+    case opts[:cni_provider]
+    when 'weave'
+      cni_network_provider = "https://cloud.weave.works/k8s/net?k8s-version=#{version}"
+      cni_pod_cidr = '10.32.0.0/12'
+    when 'flannel'
+      cni_network_provider = 'https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml'
+      cni_pod_cidr = '10.244.0.0/16'
+    when 'calico'
+      cni_network_provider = "https://docs.projectcalico.org/archive/#{opts[:cni_provider_version]}/manifests/calico.yaml"
+      cni_pod_cidr = '192.168.0.0/16'
+    when 'calico-tigera'
+      cni_network_preinstall = "https://docs.projectcalico.org/manifests/tigera-operator.yaml"
+      cni_network_provider = "https://docs.projectcalico.org/manifests/custom-resources.yaml"
+      cni_pod_cidr = '192.168.0.0/16'
+    when 'cilium'
       cni_pod_cidr = '10.244.0.0/16'
       if container_runtime.match('docker')
-        cni_network_provider = "https://raw.githubusercontent.com/cilium/cilium/#{cni_provider_version}/examples/kubernetes/#{kubernetes_minor_release}/cilium.yaml"
+        cni_network_provider = "https://raw.githubusercontent.com/cilium/cilium/#{opts[:cni_provider_version]}/examples/kubernetes/#{kubernetes_minor_release}/cilium.yaml"
       elsif container_runtime.match('crio')
-        cni_network_provider = "https://raw.githubusercontent.com/cilium/cilium/#{cni_provider_version}/examples/kubernetes/#{kubernetes_minor_release}/cilium-crio.yaml"
+        cni_network_provider = "https://raw.githubusercontent.com/cilium/cilium/#{opts[:cni_provider_version]}/examples/kubernetes/#{kubernetes_minor_release}/cilium-crio.yaml"
       end
     end
 
-
-
-    x = etcd_initial_cluster.split(",")
+    x = opts[:etcd_initial_cluster].split(",")
     cluster = String.new
     peers = String.new
     bootstrap_controller = x[0]
@@ -66,8 +61,6 @@ class OtherParams
     api_server_count = x.length
 
 
-
-
     data = Hash.new
     data['kubernetes::kubernetes_version'] = version
     data['kubernetes::kubernetes_package_version'] = kubernetes_package_version
@@ -77,13 +70,13 @@ class OtherParams
     end
     data['kubernetes::cni_network_provider'] = cni_network_provider
     data['kubernetes::cni_pod_cidr'] = cni_pod_cidr
-    data['kubernetes::cni_provider'] = cni_provider
+    data['kubernetes::cni_provider'] = opts[:cni_provider]
     data['kubernetes::etcd_initial_cluster'] = etcd_initial_cluster
     data['kubernetes::etcd_peers'] = etcd_peers
-    data['kubernetes::etcd_ip'] = etcd_ip
-    data['kubernetes::kube_api_advertise_address'] = api_address
+    data['kubernetes::etcd_ip'] = opts[:etcd_ip]
+    data['kubernetes::kube_api_advertise_address'] = opts[:api_address]
     data['kubernetes::api_server_count'] = api_server_count
-    data['kubernetes::install_dashboard'] = install
+    data['kubernetes::install_dashboard'] = opts[:install_dashboard]
     data['kubernetes::controller_address'] = controller_address
     data['kubernetes::token'] = SecureRandom.hex(3) + "." + SecureRandom.hex(8)
     File.open("kubernetes.yaml", "w+") { |file| file.write(data.to_yaml) }
